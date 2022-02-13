e8b54a64bbc7eb3e9be77ec14101d823cdf9b772
Sharad Chand <pepsighan@sharadchand.com>
2022-02-13 20:52:51 +0545

    Implement logging out

---
+++ src/components/AccountButton.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Menu, MenuItem, Typography } from '@mui/material';
+import NearIcon from './NearIcon';
+import React, { useCallback } from 'react';
+import { useAccountId, useLogout } from '../store/wallet';
+import {
+  bindMenu,
+  bindTrigger,
+  usePopupState,
+} from 'material-ui-popup-state/hooks';
+
+export default function AccountButton() {
+  const accountId = useAccountId();
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'account-menu',
+  });
+
+  const logout = useLogout();
+  const onLogout = useCallback(() => {
+    logout();
+    popupState.close();
+  }, [logout, popupState]);
+
+  return (
+    <>
+      <Button variant="contained" color="inherit" {...bindTrigger(popupState)}>
+        <Box width={24} height={24} mr={0.5}>
+          <NearIcon />
+        </Box>
+        {accountId}
+      </Button>
+
+      <Menu {...bindMenu(popupState)} elevation={1}>
+        <MenuItem onClick={onLogout} sx={{ width: 200 }}>
+          <Typography variant="body2">Logout</Typography>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,12 @@
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 import React from 'react';
-import { useAccountId } from '../store/wallet';
-import NearIcon from './NearIcon';
+import AccountButton from './AccountButton';
 
 export default function Navigation() {
-  const accountId = useAccountId();
-
   return (
     <AppBar position="static" elevation={0} color="default">
       <Toolbar sx={{ justifyContent: 'flex-end' }}>
-        <Button variant="contained" color="inherit">
-          <Box width={24} height={24} mr={0.5}>
-            <NearIcon />
-          </Box>
-          {accountId}
-        </Button>
+        <AccountButton />
       </Toolbar>
     </AppBar>
   );
