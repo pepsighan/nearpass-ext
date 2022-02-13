b740201a2c4d5e49c9b0065ba9146969da98ad95
Sharad Chand <pepsighan@sharadchand.com>
2022-02-13 14:53:36 +0545

    Setup the router

--- src/pages/Options/App.js
+++ src/pages/Options/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import theme from '../../config/theme';
 import BaseStyles from '../../components/BaseStyles';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
 
 export default function App() {
   return (
     <>
       <BaseStyles />
-      <ThemeProvider theme={theme}>Nearpass</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </HashRouter>
+      </ThemeProvider>
     </>
   );
 }
