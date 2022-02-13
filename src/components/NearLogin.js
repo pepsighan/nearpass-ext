d9f79f1b25fca91d0eff6ac4dfd5de39d3c28725
Sharad Chand <pepsighan@sharadchand.com>
2022-02-13 16:41:49 +0545

    Implement logging in

--- src/components/NearLogin.js
+++ src/components/NearLogin.js
@@ -1,8 +1,11 @@
 import { Button, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useLogin } from '../store/wallet';
 
 export default function NearLogin() {
+  const login = useLogin();
+
   return (
     <Container>
       <Helmet>
@@ -17,7 +20,7 @@ export default function NearLogin() {
           Connect your NEAR wallet to view and store your passwords
         </Typography>
 
-        <Button variant="contained" sx={{ mt: 6 }}>
+        <Button variant="contained" sx={{ mt: 6 }} onClick={login}>
           Connect with NEAR Wallet
         </Button>
       </Stack>
