a7123622509e259a3a288d27cb8e1eb3fc3f2d74
Sharad Chand <pepsighan@sharadchand.com>
2022-02-14 11:17:01 +0545

    Add button to open app

--- src/pages/Popup/App.tsx
+++ src/pages/Popup/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material';
+import React, { useCallback } from 'react';
+import { Box, Button, ThemeProvider } from '@mui/material';
 import theme from '../../config/theme';
 import BaseStyles from '../../components/BaseStyles';
 
 export default function App() {
+  const onOpenApp = useCallback(() => {
+    window.open(chrome.runtime.getURL('app.html'));
+  }, []);
+
   return (
     <>
       <BaseStyles />
-      <ThemeProvider theme={theme}>This is popup.</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ p: 2, minWidth: 200 }}>
+          <Button variant="contained" onClick={onOpenApp}>
+            Open App
+          </Button>
+        </Box>
+      </ThemeProvider>
     </>
   );
 }
