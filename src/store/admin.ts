96735ec9036181327f050826c107fe714e4a7786
Sharad Chand <pepsighan@sharadchand.com>
2022-02-14 14:23:10 +0545

    Hash the password for storage in the chain for new user

--- src/store/admin.ts
+++ src/store/admin.ts
@@ -1,4 +1,8 @@
 import create from 'zustand';
+import { useCallback } from 'react';
+import { HmacSHA512 } from 'crypto-js';
+import { useAccountId, useWallet } from './wallet';
+import networkConfig from '../config/networkConfig';
 
 type UseAdminPasswordInnerStore = {
   password: string | null;
@@ -24,4 +28,41 @@ export function storeAdminPassword(password: string) {
  */
 export function verifyAdminPassword(password: string): boolean {
   return useAdminPasswordInner.getState().password === password;
+}
+
+/**
+ * Securely store the master password such that it can be used in the process
+ * of encrypting the passwords in the manager.
+ */
+function useSecurelyStoreMasterPassword() {
+  const accountId = useAccountId();
+  const wallet = useWallet();
+
+  return useCallback(
+    async (password: string) => {
+      if (!wallet) {
+        throw new Error('Wallet not initialized yet');
+      }
+
+      const netConf = networkConfig();
+
+      // Sign the combination of accountId + password with the wallet.
+      const signature = await wallet
+        .account()
+        .connection.signer.signMessage(
+          new TextEncoder().encode(accountId + password),
+          accountId!,
+          netConf.networkId
+        );
+
+      // Hash the signature with the password.
+      const hash = HmacSHA512(
+        new TextDecoder().decode(signature.signature),
+        password
+      );
+
+      // TODO: Store the hash in the chain.
+    },
+    [wallet, accountId]
+  );
 }
