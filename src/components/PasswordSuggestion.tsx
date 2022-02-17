d603e6f87d31da543b467c1e61479cc1018a11c3
Sharad Chand <pepsighan@sharadchand.com>
2022-02-17 11:00:41 +0545

    Also show password suggestions

---
+++ src/components/PasswordSuggestion.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { List, ListItem, ListItemText, Paper, Popper } from '@mui/material';
+import { useAllSitePasswords } from '../store/sitePassword';
+
+export default function PasswordSuggestion() {
+  const [anchorEl, setAnchorEl] = useState<any>(null);
+  const open = Boolean(anchorEl);
+
+  useEffect(() => {
+    const password = document.querySelector('input[type="password"]');
+    if (!password) {
+      return;
+    }
+
+    const onFocus = () => {
+      setAnchorEl(password);
+    };
+    const onUnfocus = () => {
+      // Do not immediately close on focus out because the ListItem might have
+      // been clicked. So wait for it to be triggered.
+      setTimeout(() => setAnchorEl(null), 100);
+    };
+
+    password.addEventListener('focusin', onFocus);
+    password.addEventListener('focusout', onUnfocus);
+
+    return () => {
+      password.removeEventListener('onfocus', onFocus);
+      password.removeEventListener('focusout', onUnfocus);
+    };
+  }, [setAnchorEl]);
+
+  const { data } = useAllSitePasswords();
+  const passwords = useMemo(
+    () =>
+      data
+        .filter((it) => {
+          const hostname = new URL(it.website).hostname;
+          const currentHost = window.location.hostname;
+
+          return hostname === currentHost;
+        })
+        .map((it) => it.password),
+    [data]
+  );
+
+  return (
+    <Popper open={open && passwords.length > 0} anchorEl={anchorEl}>
+      <Paper>
+        <List sx={{ width: anchorEl?.clientWidth }}>
+          {passwords.map((it, index) => (
+            <ListItem
+              key={index}
+              button
+              onClick={() => {
+                anchorEl.value = it;
+              }}
+            >
+              <ListItemText>{'*'.repeat(it.length)}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Popper>
+  );
+}
--- src/pages/Content/App.tsx
+++ src/pages/Content/App.tsx
@@ -4,6 +4,7 @@ import theme from '../../config/theme';
 import UsernameSuggestion from '../../components/UsernameSuggestion';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useInitializeWallet } from '../../store/wallet';
+import PasswordSuggestion from '../../components/PasswordSuggestion';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <UsernameSuggestion />
+        <PasswordSuggestion />
       </ThemeProvider>
     </QueryClientProvider>
   );
