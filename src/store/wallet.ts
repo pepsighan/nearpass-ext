c4d90e64b8abebe4e42088f1bbd883305e1336fa
Sharad Chand <pepsighan@sharadchand.com>
2022-02-19 15:53:32 +0545

    Expose all the apis in the client

--- src/store/wallet.ts
+++ src/store/wallet.ts
@@ -88,12 +88,18 @@ export function useInitializeWallet() {
           'get_site_password',
           'get_all_site_password_ids',
           'get_site_passwords_by_ids',
+          'get_text',
+          'get_all_text_ids',
+          'get_texts_by_ids',
         ],
         changeMethods: [
           'initialize_account_signature',
           'add_site_password',
           'update_site_password',
           'delete_site_password',
+          'add_text',
+          'update_text',
+          'delete_text',
         ],
       });
 
