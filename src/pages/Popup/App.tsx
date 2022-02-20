e329cdf7300e307e897bf53bad4e0bbe555f66d6
Sharad Chand <pepsighan@sharadchand.com>
2022-02-20 11:12:05 +0545

    Show the form

--- src/pages/Popup/App.tsx
+++ src/pages/Popup/App.tsx
@@ -1,20 +1,52 @@
 import React, { useCallback } from 'react';
-import { Box, Button, ThemeProvider } from '@mui/material';
+import {
+  Box,
+  Button,
+  Stack,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 import theme from '../../config/theme';
 import BaseStyles from '../../components/BaseStyles';
+import { useTempSitePassword } from '../../store/tempSitePassword';
 
 export default function App() {
   const onOpenApp = useCallback(() => {
     window.open(chrome.runtime.getURL('app.html'));
   }, []);
 
+  const { website, username, password } = useTempSitePassword();
+
   return (
     <>
       <BaseStyles />
       <ThemeProvider theme={theme}>
-        <Box sx={{ p: 2, minWidth: 200 }}>
-          <Button variant="contained" onClick={onOpenApp}>
-            Open App
+        <Box sx={{ pt: 2, pb: 2, px: 2, minWidth: 300 }}>
+          <Typography variant="h6" textAlign="center">
+            Nearpass
+          </Typography>
+
+          {website && (
+            <Stack spacing={1}>
+              <TextField label="Website" value={website} disabled />
+              <TextField label="Username" value={username} disabled />
+              <TextField
+                label="Password"
+                type="password"
+                value={password}
+                disabled
+              />
+            </Stack>
+          )}
+
+          <Button
+            variant={website ? 'outlined' : 'contained'}
+            fullWidth
+            onClick={onOpenApp}
+            sx={{ mt: 1 }}
+          >
+            View Manager
           </Button>
         </Box>
       </ThemeProvider>
